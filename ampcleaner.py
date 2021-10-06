6068320ac534809f433b5b362baea8944b55398e
drnop <drnop@users.noreply.github.com>
2021-10-06 14:25:52 +0200

    Add files via upload
    some mods in ampcleaner

--- ampcleaner.py
+++ ampcleaner.py
@@ -17,14 +17,13 @@ API_KEY= "insertyourown"
 def print_help():
     print("running python " + str(sys.version_info))
     name = os.path.basename(__file__)
-    print("Usage: " + name + " -h -d debug -X -m group ")
+    print("Usage: " + name + " -h -d -v -X -t <table>")
     print(".. where options are ")
     print("  -h : help (this text)")
     print("  -v : verbose output")
     print("  -d : prints debug messages")
     print(" -t  <mac or hostname> build duplicate table based on mac or hostname")
     print("  -X : deletes the suggested duplicates")
-    print(" -m groupname : moves the suggested duplicates to group")
     print()
     print('Script assumes a file creds.json with  structure: {"cloud":"cloud-geo","api_key":"your api key","api_client_id":"your client id"')
     print('cloud-geo should be one of us or eu')
@@ -39,8 +38,6 @@ def main(argv):
     external_ip = ""
     hostname    = ""
     guid = ""
-    destgroup = ""
-    move = False
     delete = False
 
     GUIDS = {}
@@ -68,13 +65,6 @@ def main(argv):
             if table != "mac" and table != "hostname":
                 print("table should be either mac or hostname")
                 sys.exit(2)
-        if opt == ("-m"):
-            move = True
-            destgroup = arg
-      
-    if move and delete:
-        print("You cannot both move and delete duplicates!")
-        sys.exit(2)
 
     try:
         creds = json.loads(open("ampapi.json").read())
@@ -87,20 +77,22 @@ def main(argv):
         print("Ensure you have defined API_KEYs in the script for the script to work")
         sys.exit(2)
 
-    if debug:
-        print("Using API CLIENT ID {} and API KEY {}".format(API_CLIENT_ID,API_KEY))
+    if verbose:
+        print("Using CLOUD {} with API CLIENT ID {} and API KEY {}".format(CLOUD,API_CLIENT_ID,API_KEY))
     a = cats.AMP(cloud=CLOUD,api_client_id=API_CLIENT_ID,api_key=API_KEY,debug=debug,logfile="")
-
-    if move:
-        rsp = a.groups(groupname=destgroup)
-        print(json.dumps(rsp,indent=4,sort_keys=True))
 
     #get all computers
     rsp = a.computers(internal_ip=internal_ip,external_ip=external_ip,hostname=hostname)
     data = rsp["data"]
 
+    print("Number of cmmputers returned: {}".format(str(len(data))))
     # loop through all computers and store in GUIDS, HOSTNAMES, and MACS
+    count = 0
     for computer in data:
+        count = count +1
+        if verbose and count % 30 == 0:
+            print("Processing number {}".format(str(count)))
+        # iteration handling one Computer (identified by GUID) - populate the details for this computer
         connector_guid = computer["connector_guid"]
         hostname = computer["hostname"]
         if "network_addresses" in computer:
@@ -116,36 +108,45 @@ def main(argv):
             "last_seen" : last_seen,
             "install_date" : install_date
         }
+        ## append the details to the table keyed on GUIDs, should have no duplicates
         if connector_guid in GUIDS:
-            print("found duplicate GUIID "+guid)
+            print("found duplicate GUID "+guid)
+            print("This should never happen!!!")
             GUIDS[connector_guid].append(details)
         else:
             GUIDS[connector_guid] = [details]
-           
+        
+        ## add the details to the table for hostnames, this is a structure keyed on hostnames with a list of details
+        ## if there are no duplicates (only one GUID for the hostname, the list will only contain one entry)
         if hostname in HOSTNAMES:
             print("found duplicate HOSTNAME "+hostname)
             HOSTNAMES[hostname].append(details)
         else:
             HOSTNAMES[hostname] = [details]
-        #print(json.dumps(computer,indent=4,sort_keys=True))
+
+        ## add the details to the table for MAC addresses, this is a structure keyed on MACs witha list of details
+        ## if there are no duplicates (only one GUID for the same hostname, the list will contain only one entry)
+        ## we have to interate through each of the MAC addresses of the retrieved computer since there may be more than one
         if "network_addresses" in computer:
             network_addresses = computer["network_addresses"]
             for network_address in network_addresses:
+                # iterate through all the interface card of the computer, each with different mac
                 mac = network_address["mac"]
                 if mac in MACS:
+                    ## the mac has been previously stored, check if we have a new GUID
                     this_mac = MACS[mac]
                     for m in this_mac:
                         if connector_guid != m["guid"]:
                             MACS[mac].append(details)
-                            print("found duplicate MAC "+mac)
+                            print("found duplicate MAC with a different GUID "+mac)
                             break
                         else:
-                            print("found duplicate MAC  - but with same Connector Guid:"+mac)
+                            print("found duplicate MAC  - but with same GUID:"+mac)
 
                 else:
                     MACS[mac] = [details]
         
-    print("GUIDS")
+    print("GUIDS table contains {} entries".format(str(len(GUIDS))))
     for item in GUIDS:
         if len(GUIDS[item]) >1:
             print("Duplicate Found for GUID: Should not happen!!!! " + item)
@@ -160,28 +161,41 @@ def main(argv):
         TABLE = MACS
         print("CHECKING FOR DUPLICATE MACS")
         itemname = "MAC"
+    print("{} contains {} entries".format(itemname,str(len(TABLE))))
+    duplicates_found = 0
+    duplicates_deleted = 0
     for item in TABLE:
         if TABLE[item] and len(TABLE[item]) >1:
-            print("-----------------------------------")
+            print()
+            print("-------------------------------------------------------")
             print("Duplicate(s) Found for {} : {}".format(itemname,item))
-            print("-----------------------------------")
+            print("-------------------------------------------------------")
             sorted_list = sorted(TABLE[item],key=itemgetter("last_seen"),reverse=True)
+            duplicates_found = duplicates_found + len(sorted_list)-1
             most_recent = True
             for s in sorted_list:
                 #print(json.dumps(s,indent=4,sort_keys=True))
                 if most_recent:
                     print("Most recently seen with this {}:".format(itemname))
+                    print()
                 else:
-                   
-                    print("Likely stale duplicate of the most recently seen computer with this MAC")
+                    print("Likely stale duplicate of the most recently seen computer with this {}".format(itemname))
                 print("GUID: {} SEEN: {} INSTALLED: {} HOSTNAME:{} MACS:{}".format(s["guid"],s["last_seen"],s["install_date"],s["hostname"],str(s["network_addresses"])))
-                
+                print()
                 if not most_recent and delete:
-                    answer = input("Delete this computer with GUID {}  [Y/y]".format(s["guid"]))
+                    answer = input("Delete this computer with GUID {}  [Y/y] [Q/q] for quit, any other character to go to next item: ".format(s["guid"]))
+                    if (answer == "q" or answer =="Q"):
+                        print("quitting before finding all duplicates....")
+                        return()
                     if (answer == "y" or answer == "Y"):
                         dum = a.computerDelete(guid=s["guid"])
+                        duplicates_deleted = duplicates_deleted + 1
                         
                 most_recent = False
+    print()
+    print("*********** SUMMARY *************************")
+    print("Found {} duplicates: ".format(str(duplicates_found)))
+    print("Deleted {} duplicates: ".format(str(duplicates_deleted)))
 
    
 if __name__ == "__main__":
